ac8c6f98eb8af144bbdff753d41ce3f5f6ed085d
pphuc25 <phanphuc1100@gmail.com>
2022-11-22 23:19:55 +0700

    Update fit and predict method, intercept and coefficient attribute

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ One thing you need to know that, I make this project **just for learning and und
 
 ### Regression
 - [x] Linear Regression
+    - *22 Nov, 2022*: Update fit and predict method, intercept and coefficient attribute
 - [ ] Ridge Regression
 - [ ] Lasso Regression
 - [ ] Decision Tree for Regression
--- regression/LinearRegression.py
+++ regression/LinearRegression.py
@@ -8,28 +8,35 @@ class LinearRegression:
         """Find the linear that fit with data given 
         :param
             random_init: bool
-                set the permanent random or not 
-
+                set the permanent random or not
+        :attribute
+            _coef: np.ndarray
+                the coefficient
+            _inte: float
+                the intercept
         """
-        if random_init: self = np.random.seed(99)
-    
-    def fit(self, x: Union[List[List], np.ndarray], y: Union[List, np.ndarray]) -> np.ndarray:
+        if random_init: np.random.seed(99)
+        self._coef = None
+        self._inte = None
+
+    def fit(self, x: Union[List[List], np.ndarray], y: Union[List, np.ndarray]) -> None:
         """Calculate the weight parameter with independent data (x) and dependent data(y)
         :param
             x: List[List], np.ndarray
                 independent data
             y: List, np.ndarray
                 dependent data
-        :return
-            ndarray: weight parameter(s) was calculated by x and y
-
         """
         x, y = np.array(x), np.array(y)
         assert np.ndim(x) == 2, Exception('ndim of x must be 2')
         assert np.ndim(y) == 1, Exception('ndim of y must be 1')
+        assert x.shape[0] == y.shape[-1], Exception('x and y must have same size')
+
+        ones = np.ones([x.shape[0], 1])
+        x = np.concatenate([ones, x], axis=1)
         theta = pinv(x.T@x) @ (x.T@y)
-        self._theta = theta
-        return self._theta
+        
+        self._inte, self._coef = theta[0], theta[1:]
 
     def predict(self, x_pred: Union[List[List], np.ndarray]) -> np.ndarray:
         """Predict value from the data given (predict y from x)
@@ -38,10 +45,10 @@ class LinearRegression:
                 independent data
         :return
             ndarray: predictive value calculate from x_pred and weight
-        
         """
         x_pred = np.array(x_pred)
         assert np.ndim(x_pred) == 2, Exception('ndim of x must be 2')
-        # assert self._theta, Exception("haven't fit data yet")
-        y_pred = np.dot(x_pred, self._theta)
+        assert self._coef is not None and \
+               self._inte is not None, Exception("haven't fit data yet")
+        y_pred = np.dot(x_pred, self._coef) + self._inte
         return y_pred
