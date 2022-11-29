b5b5bde4a658b4c42e5efbd3bd3a5296141b5038
pphuc25 <phanphuc1100@gmail.com>
2022-11-29 23:36:43 +0700

    need to fix bug

--- classification/LogisticRegression.py
+++ classification/LogisticRegression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class LogisticRegression:
     def __init__(self, random_state: bool = False) -> None:
@@ -16,8 +17,11 @@ class LogisticRegression:
     def sigmoid_backward(self, X):
         return X*(1-X)
 
-    def binary_cross_entropy(self, y, y_pred):
-        loss = -(y.T@np.log(y_pred) + (1-y.T)@np.log(1-y_pred))
+    def binary_cross_entropy(self, y, y_pred, batch_size):
+        y_pos = np.min(np.concatenate([y_pred + 1e-9, np.ones_like(y_pred)],axis=1))
+        y_neg = np.min(np.concatenate([1-y_pred + 1e-9, np.ones_like(y_pred)],axis=1))
+        loss = -(y*np.log(y_pos) + (1-y)*np.log(1-y_neg)) / batch_size
+
         dy_loss = -(y/y_pred - (1-y)/(1-y_pred))
         return loss, dy_loss
 
@@ -25,9 +29,8 @@ class LogisticRegression:
         return np.random.permutation(x.shape[0])
 
     def fit(self, x, y, learning_rate, epochs, batch):
-        ones = np.ones((x.shape[0], 1))
         self.theta = np.random.rand(x.shape[1], 1)
-        loss_graph = []
+        self.loss_graph = []
         for ith_epoch in range(epochs):
             shuffle = self.__shuffle_index(x)
             x, y = x[shuffle], y[shuffle]
@@ -35,46 +38,25 @@ class LogisticRegression:
                 logits = self.linear_forward(x[ith_batch:ith_batch+batch, :], self.theta)
                 y_pred = self.sigmoid_forward(logits)
 
-                loss, dy_loss = self.binary_cross_entropy(y[ith_batch:ith_batch+batch, :], y_pred)
+                loss, dy_loss = self.binary_cross_entropy(y[ith_batch:ith_batch+batch, :], y_pred, batch)
 
-                print(dy_loss.shape)
-                a = self.linear_backward(x[ith_batch:ith_batch+batch, :]).shape
-                b = self.sigmoid_backward(y_pred).shape
-                print(a, b)
+                linear_backward = self.linear_backward(x[ith_batch:ith_batch+batch, :])
+                sigmoid_backward = self.sigmoid_backward(y_pred)
 
-                dtheta = dy_loss*self.linear_backward(x[ith_batch:ith_batch+batch, :])*self.sigmoid_backward(y_pred)
+                dtheta = dy_loss*sigmoid_backward*linear_backward
+                dtheta = np.mean(dtheta)
                 self.theta = self.theta - learning_rate*dtheta
 
             if ith_epoch%10 == 0:
-                print(f'loss of {ith_epoch} is: {loss}')
-                loss_graph.append(loss)
+                # print(f'loss of {ith_epoch} is: {loss}')
+                self.loss_graph.append(loss)
+
+    def predict(self, x_test):
+        logits = self.linear_forward(x_test, self.theta)
+        return self.sigmoid_forward(logits)
 
         
 
-
-    # def train(self, X, y, theta, b, learning_rate, epochs):
-    #     loss_graph = []
-    #     for ith_epoch in range(epochs):
-    #         shuffle_index = np.arange(X.shape[0])
-    #         np.random.shuffle(shuffle_index)
-            
-    #         for i in shuffle_index:
-    #             logits = self.linear_forward(X[i], theta, b)
-    #             y_pred = self.sigmoid_forward(logits)
-
-    #             loss, dy_pred = self.binary_cross_entropy(y[i], y_pred)
-
-    #             dtheta = dy_pred * self.linear_backward(X[i])[0] * self.sigmoid_backward(y_pred)
-    #             db = dy_pred * self.linear_backward(X[i])[1] * self.sigmoid_backward(y_pred)
-
-    #             theta = theta - learning_rate*dtheta
-    #             b = b - learning_rate*db
-            
-    #         if ith_epoch % 30 == 0:
-    #             print('loss: ', ith_epoch, 'is' , float(loss))
-    #             loss_graph.append(loss)
-
-    #     return theta, b
 
 
                 
@@ -88,19 +70,24 @@ Y = [np.zeros_like(x_0), np.ones_like(x_1)]
 
 X, Y = np.concatenate(X), np.concatenate(Y)
 
+
 X, Y = np.reshape(X, (-1, 1)), np.reshape(Y, (-1, 1))
 
 ones = np.ones((X.shape[0], 1))
 X = np.concatenate((ones, X), axis=1)
-Y = np.concatenate((ones, Y), axis=1)
 
-epochs = 120
+epochs = 1000
 lr = 0.001
 batch = 256
 
 reg = LogisticRegression()
 reg.fit(X, Y, lr, epochs, batch)
+print(reg.loss_graph)
 
-print(reg.theta)
+plt.plot(reg.loss_graph)
+plt.show()
+
+print(reg.predict(X[10:20]))
+print(Y[10:20])
 
         
